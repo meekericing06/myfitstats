a7131ddfa164c945d030b1efe85ae0bd673ac563
Sasanka <rsasanka@gmail.com>
2026-02-14 13:44:50 -0800

    Test

---
+++ myhealthapp/myhealthapp/myhealthappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myhealthappApp.swift
+//  myhealthapp
+//
+//  Created by Sasanka R on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct myhealthappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
